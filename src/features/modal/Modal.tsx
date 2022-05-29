b77b312bd2d4ffceed948eeadb007759fc6b9153
be-student <presentsong@naver.com>
2022-05-29 23:05:15 +0900

    save

--- src/features/modal/Modal.tsx
+++ src/features/modal/Modal.tsx
@@ -1,17 +1,38 @@
 import { useState } from "react";
-import { useInput } from "../../app/hooks";
+import { useAppDispatch, useInput } from "../../app/hooks";
 import styles from "./Modal.module.css";
-
+import Calendar from "react-calendar";
+import moment from "moment";
+import { addTask } from "../task/taskSlice";
 const Modal = ({ setModal }) => {
+  const dispatch=useAppDispatch();
   const title = useInput("");
   const [goalAt, setGoalAt] = useState<Date>(new Date());
   const description = useInput("");
   const [complete, setComplete] = useState<boolean>(false);
+  const onSubmit=()=>{
+    if(title.value===""){
+      alert("please enter title");
+      return;
+    }
+    if(description.value===""){
+      alert("please enter description");
+      return;
+    }
+    console.log(typeof new Date(goalAt).valueOf());
+    dispatch(addTask({
+      title:"testing Task",
+      description:"testing Description",
+      goalAt:new Date(goalAt),
+      Complete:true,
+      tag:[]
+    }))
+  }
   return (
     <div className={styles.ModalWrapper}>
       <div className={styles.ModalItem}>
         <h1 className={styles.Title}>Task</h1>
-        <div className={styles.}>
+        <div className={styles.Button}>
           <span>Title </span>
           <input
             value={title.value}
@@ -29,9 +50,9 @@ const Modal = ({ setModal }) => {
             required
           />
         </div>
-        <div>
-          <span>Goal </span>
-          <DatePicker selected={goalAt} onChange={(date) => setGoalAt(date)} />
+        <div style={{height:"250px"}}>
+          <span>Goal {moment(goalAt).format("YYYY-MM-DD")}</span>
+          <Calendar  value={goalAt} onChange={setGoalAt}></Calendar>
         </div>
 
         <div>
@@ -46,10 +67,14 @@ const Modal = ({ setModal }) => {
         </div>
 
         <div>
-          <span>Tag </span>
-          <input type="checkbox" />
+          <span>Tag</span>          
         </div>
-
+        <div>
+                  
+        </div>
+        <div>
+          <button onClick={onSubmit}>submit</button>
+        </div>
         <button
           type="button"
           onClick={() => {
